48ca9e05af1d8fdc94cfa1c54b387e1ab0a64562
seductorAmadeus <seductorAmadeus@gmail.com>
2017-01-08 09:11:10 +0300

    Fix problem with the addition of the element in the top of the list

--- src/main.c
+++ src/main.c
@@ -7,9 +7,13 @@ typedef struct llist_t
     struct llist_t *next;
 } llist_t;
 
+long list_sum(llist_t *list);
+
+llist_t *list_add_front(int number, llist_t **head);
+
 llist_t *list_create(int number);
 
-llist_t *list_add_front(int number, llist_t *llist);
+int list_length(llist_t *list);
 
 void list_add_back(int number, llist_t *llist);
 
@@ -17,54 +21,62 @@ int list_get(int i);
 
 void list_free(llist_t *llist);
 
-int list_lenght(llist_t *llist);
-
 llist_t *list_node_at(llist_t llist, int index);
-
-long list_sum(llist_t *llist);
 
 int main()
 {
     int scanValue;
-    llist_t *llist = NULL;
+    llist_t *list = NULL;
     while (scanf("%d", &scanValue) != EOF)
     {
-        if (llist == NULL)
+        if (NULL == list)
         {
-            llist = list_create(scanValue);
+            list = list_create(scanValue);
         } else
         {
-            llist = list_add_front(scanValue, llist);
+            list = list_add_front(scanValue, &list);
         }
     }
-    printf("%ld", list_sum(llist));
-
+    printf("%ld\n", list_sum(list));
+    printf("length of list = %d\n", list_length(list));
     return 0;
 }
 
-long list_sum(llist_t *llist)
+long list_sum(llist_t *list)
 {
     long sum = 0;
-
-    while (NULL != llist)
+    while (NULL != list)
     {
-        sum += llist->value;
-        llist = llist->next;
+        sum += list->value;
+        list = list->next;
     }
-
     return sum;
+}
+
+llist_t *list_add_front(int number, llist_t **head)
+{
+    llist_t *tmp = malloc(sizeof(llist_t));
+    tmp->value = number;
+    tmp->next = *head;
+    *head = tmp;
+    return *head;
 }
 
 llist_t *list_create(int number)
 {
-    return list_add_front(number, NULL);
+    llist_t *head = (llist_t *) malloc(sizeof(llist_t));
+    head->value = number;
+    head->next = NULL;
+    return head;
 }
 
-llist_t *list_add_front(int number, llist_t *llist)
+int list_length(llist_t *list)
 {
-    llist_t *next;
-    next = malloc(sizeof(llist_t));
-    next->value = number;
-    next->next = llist;
-    return next;
+    int list_length = 0;
+    while (NULL != list)
+    {
+        list_length++;
+        list = list->next;
+    }
+    return list_length;
 }
